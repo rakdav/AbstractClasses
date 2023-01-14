45bb32c4e4ec07b1121218e131ab411c6492e4d6
Andrews <rakdav@inbox.ru>
2023-01-14 09:58:02 +0200

    last

--- AbstractClasses/Program.cs
+++ AbstractClasses/Program.cs
@@ -7,3 +7,20 @@ double height = double.Parse(Console.ReadLine());
 Rectangle rectangle=new Rectangle(width, height);
 Console.WriteLine($"Площадь:{rectangle.Area():F2}, " +
     $"периметр:{rectangle.Perimeter():F2}");
+Console.WriteLine("Введите радиус:");
+double radius = double.Parse(Console.ReadLine());
+Circle circle=new Circle(radius);
+Console.WriteLine($"Площадь:{circle.Area():F2}, " +
+    $"периметр:{circle.Perimeter():F2}");
+Console.WriteLine("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите c:");
+double c = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите h:");
+double h = double.Parse(Console.ReadLine());
+Trapezium trapezium = new Trapezium(a, b, c, h);
+Console.WriteLine($"Площадь:{trapezium.Area():F2}, " +
+    $"периметр:{trapezium.Perimeter():F2}");
+
---
+++ AbstractClasses/Trapezium.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Trapezium:Figure
+    {
+        private double a;
+        private double b;
+        private double c;
+        private double h;
+        public double A
+        {
+            get { return a; }
+            set { if(value>0) a = value; }
+        }
+        public double B
+        {
+            get { return b; }
+            set { if (value > 0) b = value; }
+        }
+        public double C
+        {
+            get { return c; }
+            set { if (value > 0) c = value; }
+        }
+        public double H
+        {
+            get { return h; }
+            set { if (value > 0) h = value; }
+        }
+
+        public Trapezium(double a, double b, double c, double h)
+        {
+            A = a;
+            B = b;
+            C = c;
+            H = h;
+        }
+
+        public override double Area()
+        {
+            return (a+b)*h/2;
+        }
+
+        public override double Perimeter()
+        {
+            return a+b+2*c;
+        }
+    }
+}
